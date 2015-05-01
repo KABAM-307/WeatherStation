7cbb6a89dbddd379079beb3c57d8f566f784ed63
Matt Molo <molo.matt@gmail.com>
2015-05-01 17:38:51 -0400

    historical data

--- Weather Station/src/weatherStation/ResponseHandler.java
+++ Weather Station/src/weatherStation/ResponseHandler.java
@@ -15,7 +15,7 @@ class ResponseHandler implements HttpHandler {
 
  	//Responds to web request with the weather stations response
 	public void handle(HttpExchange t) throws IOException {
-		String response = ws.createDataJSON();
+		String response = ws.getHistoricalJSON(10);
 		System.out.println(response);
 		Headers headers = t.getResponseHeaders();
 		headers.add("Content-Type", "application/json");
